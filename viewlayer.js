d25c94923c72c04dbf3329400581213e228b86e4
arkan <arkan@xtremax.com>
2018-02-05 06:55:54 +0700

    learn get and post uing express

--- viewlayer.js
+++ viewlayer.js
@@ -1,49 +1,61 @@
 const path = require("path");
 const express = require("express");
-const data = require('./data/items.json')
-console.log(data)
-
+const data = require('./data/item.json')
+const bodyParser = require('body-parser')
 const app = express();
 
 app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "hbs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 const middleWareSatu = (req, res, next) => {
-  console.log("middleware pertama");
-  next();
+    console.log("middleware pertama");
+    next();
 };
 
 const middleWareDua = (req, res, next) => {
-  console.log("middleware kedua");
-  next();
+    console.log("middleware kedua");
+    next();
 };
 
 app.use(middleWareSatu);
 app.use(middleWareDua);
 
 app.get("/", (request, response) => {
-  // response.send('index');
-  response.render("index", {
-    greetings: "halooo anjir"
-  });
+    // response.send('index');
+    response.render("index", {
+        items: data
+    });
 });
 
+app.get('/echo', (req, res) => {
+    res.render('post')
+})
+app.post('/echo', (req, res) => {
+    res.render('post', {
+        name: req.body.TxtName
+    })
+
+})
+
 app.get("/echo/:name", (req, res) => {
-  //res.send('this is from echo routes calling:' + req.params.name)
-  res.render("index", {
-    greetings: req.params.name
-  });
+    //res.send('this is from echo routes calling:' + req.params.name)
+    res.render("index", {
+        greetings: req.params.name
+    });
 });
 
 app.get("/hactive8", (req, res) => {
-  res.redirect("https://hacktiv8.com/");
+    res.redirect("https://hacktiv8.com/");
 });
 
 app.get("/download", (req, res) => {
-  res.sendFile("E:DownloadsMusicRingtoneLocked_Away.mp3");
+    res.sendFile("E:DownloadsMusicRingtoneLocked_Away.mp3");
 });
 
 app.listen(5443, () => {
-  console.log("executed on 127.0.0.1");
+    console.log("executed on 127.0.0.1");
 });
